049baadb0fd81cccd0bab528b3f4b4983ef32e88
Sleeplessmen <124755138+Sleeplessmen@users.noreply.github.com>
2026-05-06 12:52:05 +0700

    minor changes

--- nextjs/src/components/providers/modal-provider.tsx
+++ nextjs/src/components/providers/modal-provider.tsx
@@ -1,27 +1,27 @@
 "use client";
 
 import { useEffect, useState } from "react";
-// Import your custom modals here
+
 // import { CardModal } from "@/components/modals/card-modal"; 
 // import { ProModal } from "@/components/modals/pro-modal";
 
 export const ModalProvider = () => {
-  const [isMounted, setIsMounted] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
 
-  // Prevent Hydration Errors: Only render modals after the component 
-  // has mounted on the client.
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+    // Prevent Hydration Errors: Only render modals after the component 
+    // has mounted on the client.
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
-  if (!isMounted) {
-    return null;
-  }
+    if (!isMounted) {
+        return null;
+    }
 
-  return (
-    <>
-      {/* <CardModal /> */}
-      {/* <ProModal /> */}
-    </>
-  );
+    return (
+        <>
+            {/* <CardModal /> */}
+            {/* <ProModal /> */}
+        </>
+    );
 };
